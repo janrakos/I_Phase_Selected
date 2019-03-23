605b21b83fd8e8525d5ae3a3f2abd7d50be14795
Jan Rákos <janrkos@gmail.com>
2019-03-23 12:00:04 +0100

    farmated scripts

--- src/functions.ps1
+++ src/functions.ps1
@@ -1,67 +1,120 @@
 function Main () {
-  $ErrorActionPreference = "Stop"
-  $mainFolder = "C:\git\I_Phase_selected"
-  $outputFolder = setUpDir "output"
-  $logFileFolder = setUpDir "log"
-  $logFile = createLogFile
-  try {
-  validateParameters
-  $inputFolder = setUpInputDir
-  $allFiles = Get-ChildItem -Path $inputFolder | Where-Object { !$_.PSIsContainer } | Sort-Object
-  Remove-Item "$outputFolder\*.*"
-  if ($isConversionWanted -eq $true -and $doOnlyConversion -eq $true) {
-    foreach ($file in $allFiles) {
-      copyFileToFolder $file $inputFolder $outputFolder
+    $ErrorActionPreference = "Stop"
+    $outputFolder = setUpDir "output"
+    $logFileFolder = setUpDir "log"
+    $logFile = createLogFile
+    try {
+        validateParameters
+        $inputFolder = setUpInputDir
+        $allFiles = Get-ChildItem -Path $inputFolder | Where-Object { !$_.PSIsContainer } | Sort-Object
+        Remove-Item "$outputFolder\*.*"
+        if ($isConversionWanted -eq $true -and $doOnlyConversion -eq $true) {
+            foreach ($file in $allFiles) {
+                copyFileToFolder $file $inputFolder $outputFolder
+            }
+        }
+        else {
+            $scriptStartString = "MD_SCRIPT_TIMES.SCRIPT_START"
+            $scriptEndString = "MD_SCRIPT_TIMES.SCRIPT_END"
+            $copyStringsArray = createArrayFromString $copyStrings
+            $ignoreStringsArray = createArrayFromString $ignoreStrings
+            foreach ($file in $allFiles) {
+                $fileName = $file.Name
+                $scriptStart = getRowNumberOfText $file $scriptStartString
+                $scriptEnd = getRowNumberOfText $file $scriptEndString
+                $isAnyCopyStringInFile = isAnyStringFromArrayInFile $file $copyStringsArray
+                $isAnyIgnoreStringInFile = isAnyStringFromArrayInFile $file $ignoreStringsArray
+                if ($isAnyIgnoreStringInFile -eq $true) {
+                    logWrite "Script $fileName contains one or more ignore strings. Script was ignored and was not processed or copied to output."
+                }
+                elseif ($isAnyCopyStringInFile -eq $true) {
+                    copyFileToFolder $file $inputFolder $outputFolder
+                    logWrite "Script $fileName contains one or more copy strings. Script was copied from input folder without processing."
+                }
+                elseif ($ScriptStart -eq $null -or $ScriptEnd -eq $null -or $scriptStart -eq "MultipleRowsFoundError" -or $scriptEnd -eq "MultipleRowsFoundError") {
+                    copyFileToFolder $file $inputFolder $outputFolder
+                    logWrite "WARNING: Problem in finding Start or End of skript $fileName. Script was copied from input folder without processing."
+                }
+                else {
+                    processFile $file
+                }
+            }
+        }
+        $processedFiles = Get-ChildItem -Path $outputFolder | Where-Object { !$_.PSIsContainer } | Sort-Object
+        if ($createFilesForIFPC -eq $true) {
+            foreach ($file in $processedFiles) {
+                copyFileToFolder $file $outputFolder $IFPCFolder
+            }
+        }
     }
-  }
-  else {
-    $scriptStartString = "MD_SCRIPT_TIMES.SCRIPT_START"
-    $scriptEndString = "MD_SCRIPT_TIMES.SCRIPT_END"
-    $copyStringsArray = createArrayFromString $copyStrings
-    $ignoreStringsArray = createArrayFromString $ignoreStrings
-    foreach ($file in $allFiles) {
-      $fileName = $file.Name
-      $scriptStart = getRowNumberOfText $file $scriptStartString
-      $scriptEnd = getRowNumberOfText $file $scriptEndString
-      $isAnyCopyStringInFile = isAnyStringFromArrayInFile $file $copyStringsArray
-      $isAnyIgnoreStringInFile = isAnyStringFromArrayInFile $file $ignoreStringsArray
-      if ($isAnyIgnoreStringInFile -eq $true) {
-        logWrite "Script $fileName contains one or more ignore strings. Script was ignored and was not processed or copied to output."
-      }
-      elseif ($isAnyCopyStringInFile -eq $true) {
-        copyFileToFolder $file $inputFolder $outputFolder
-        logWrite "Script $fileName contains one or more copy strings. Script was copied from input folder without processing."
-      }
-      elseif ($ScriptStart -eq $null -or $ScriptEnd -eq $null -or $scriptStart -eq "MultipleRowsFoundError" -or $scriptEnd -eq "MultipleRowsFoundError") {
-        copyFileToFolder $file $inputFolder $outputFolder
-        logWrite "WARNING: Problem in finding Start or End of skript $fileName. Script was copied from input folder without processing."
-      }
-      else {
-        processFile $file
-      }
+    Catch {
+        $ErrorMessage = $_.Exception.Message
+        logWrite "ERROR: $ErrorMessage"
+        logWrite "Terminating."
     }
-  }
-  $processedFiles = Get-ChildItem -Path $outputFolder | Where-Object { !$_.PSIsContainer } | Sort-Object
-  if ($createFilesInMainFolder -eq $true) {
-    foreach ($file in $processedFiles) {
-      copyFileToFolder $file $outputFolder $mainFolder
-    }
-  }
-}
-
-Catch {
-  $ErrorMessage = $_.Exception.Message
-  logWrite "ERROR: $ErrorMessage"
-  logWrite "Terminating."
-}
 }
 
 function setUpDir ($dir) {
     if (!(Test-Path "$mainFolder\$dir")) {
         New-Item "$mainFolder\$dir" -itemtype directory
     }
-    $outputFolder = "$mainFolder\$dir"
-    return $outputFolder
+    $settedUpFolder = "$mainFolder\$dir"
+    return $settedUpFolder
+}
+
+function createLogFile () {
+    $Stamp = (Get-Date).toString("dd-MM-yyyy HHmmss")
+    $logFileName = "run." + $Stamp -replace " ","."
+    $newLogFile = New-Item "$logFileFolder\$logFileName.log" -ItemType file 
+    return $newLogFile
+}
+
+function logWrite ([string]$message) {
+    $Stamp = (Get-Date).toString("dd.MM.yyyy HH:mm:ss")
+    Add-Content $logFile -Value "$Stamp     $message"
+    Write-Host $message
+}
+
+function validateParameters () {
+    if ($sourceSystem -in $null,"" -or !($SourceSystem -is [string])) {
+        throw "Parametr `$sourceSystem does not have valid value. Check documentation for more information about this parametr."
+    }
+    if (!($copyStrings -is [string])) {
+        throw "Parametr `$copyStrings does not have valid value. Check documentation for more information about this parametr."
+    }
+    if (!($ignoreStrings -is [string])) {
+        throw "Parametr `$ignoreStrings does not have valid value. Check documentation for more information about this parametr."
+    }
+    if (!($inputFilesEncoding.ToLower() -in "unknown", "string", "unicode", "bigendianunicode", "utf8", "utf7", "utf32", "ascii", "default", "oem" )) {
+        throw "Parametr `$inputFilesEncoding does not have valid value. Check documentation for more information about this parametr."
+    }
+    if (!($fromOwnInputFolder -is [bool])) {
+        throw "Parametr `$fromOwnInputFolder is not set properly. It's value have to be $true or $false."
+    }
+    if (!(Test-Path $ownInputFolder) -and $fromOwnInputFolder -eq $true) {
+        throw "Input folder $ownInputFolder does not exist."
+    }
+    if (!(Test-Path "$mainFolder\input") -and $fromOwnInputFolder -eq $false) {
+        throw "Input folder $mainFolder\input does not exist."
+    }
+    if (!($createFilesForIFPC -is [bool])) {
+        throw "Parametr `$createFilesForIFPC is not set properly. It's value have to be $true or $false."
+    }
+    if (!(Test-Path $IFPCFolder) -and $createFilesForIFPC -eq $true) {
+        throw "Input folder $IFPCFolder does not exist."
+    }
+    if (!($isConversionWanted -is [bool])) {
+        throw "Parametr `$isConversionWanted is not set properly. It's value have to be $true or $false."
+    }
+    if (!($doOnlyConversion -is [bool])) {
+        throw "Parametr `$doOnlyConversion is not set properly. It's value have to be $true or $false."
+    }
+    if (!($inputFilesEncoding.ToLower() -in "unknown", "string", "unicode", "bigendianunicode", "utf8", "utf7", "utf32", "ascii", "default", "oem" )) {
+        throw "Parametr `$inputFilesEncoding does not have valid value. Check documentation for more information about this parametr."
+    }
+    if (!(Test-Path "$mainFolder\src\cfg\$ConversionDefinitionFile") -and $isConversionWanted -eq $true -and $ownConversionDefinitionFile -eq $true) {
+        throw "File $ConversionDefinitionFile does not exist."
+    }
 }
 
 function setUpInputDir () {
@@ -74,93 +127,25 @@ function setUpInputDir () {
     return $inputFolder
 }
 
-function validateParameters () {
-  if (!($fromOwnInputFolder -is [bool])) {
-    throw "Parametr `$fromOwnInputFolder is not set properly. It's value have to be $true or $false."
-  }
-  if (!(Test-Path $ownInputFolder) -and $fromOwnInputFolder -eq $true) {
-    throw "Input folder $ownInputFolder does not exist."
-  }
-  if (!(Test-Path "$mainFolder\input") -and $fromOwnInputFolder -eq $false) {
-    throw "Input folder $mainFolder\input does not exist."
-  }
-  if ($sourceSystem -in $null,"" -or !($SourceSystem -is [string])) {
-    throw "Parametr `$sourceSystem does not have valid value. Check documentation for more information about this parametr."
-  }
-  if (!($copyStrings -is [string])) {
-    throw "Parametr `$copyStrings does not have valid value. Check documentation for more information about this parametr."
-  }
-  if (!($ignoreStrings -is [string])) {
-    throw "Parametr `$ignoreStrings does not have valid value. Check documentation for more information about this parametr."
-  }
-  if (!($inputFilesEncoding.ToLower() -in "unknown", "string", "unicode", "bigendianunicode", "utf8", "utf7", "utf32", "ascii", "default", "oem" )) {
-    throw "Parametr `$inputFilesEncoding does not have valid value. Check documentation for more information about this parametr."
-  }
-  if (!($createFilesInMainFolder -is [bool])) {
-    throw "Parametr `$createFilesInMainFolder is not set properly. It's value have to be $true or $false."
-  }
-  if (!($isConversionWanted -is [bool])) {
-    throw "Parametr `$isConversionWanted is not set properly. It's value have to be $true or $false."
-  }
-  if (!($doOnlyConversion -is [bool])) {
-    throw "Parametr `$doOnlyConversion is not set properly. It's value have to be $true or $false."
-  }
-  if (!(Test-Path "$mainFolder\src\cfg\$conversionDefinitionFile") -and $isConversionWanted -eq $true) {
-    throw "$conversionDefinitionFile does not exist in configuration folder $mainFolder\src\cfg\ ."
-  }
-}
-
-function getRowNumberOfText ($file, $search) {
-  $searchedLine = Get-Content "$inputFolder\$file" -encoding $inputFilesEncoding | Select-String $search
-  if ($searchedLine -eq $null) {
-    return $null
-  }
-  elseif ($searchedLine.count -gt 1) {
-    return "MultipleRowsFoundError"
-  }
-  else {
-    [int] $lineNumber = $searchedLine.LineNumber
-    return $lineNumber
-  }
-}
-
 function deleteFilefromFolder ($file, $folder) {
-  $fileName = $file.Name
-  if (Test-Path "$folder\$fileName") {
-    Remove-Item "$folder\$fileName"
-  }
+    $fileName = $file.Name
+    if (Test-Path "$folder\$fileName") {
+        Remove-Item "$folder\$fileName"
+    }
 }
 
 function copyFileToFolder ($file, $sourceFolder, $targetFolder) {
-  $fileName = $file.Name
-  deleteFilefromFolder $file $targetFolder
-  Copy-Item -Path "$sourceFolder\$fileName" -Destination $targetFolder
-}
-
-function createEmptiedFileInFolder ($file, $sourceFolder, $targetFolder) {
     $fileName = $file.Name
-    copyFileToFolder $file $sourceFolder $targetFolder
-    Clear-Content "$targetFolder\$fileName"
-}
-
-function createFileFromArray ($file, $array, $folder) {
-  $fileName = $file.Name
-  if ($inputFilesEncoding.ToLower() -eq 'utf8') {
-  $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
-  [System.IO.File]::WriteAllLines("$folder\$fileName", $array, $Utf8NoBomEncoding)
-  }
-  else {
-  $newFile = New-Item "$folder\$fileName" -ItemType file 
-  $array | Out-File $newFile -Encoding $inputFilesEncoding
-  }
+    deleteFilefromFolder $file $targetFolder
+    Copy-Item -Path "$sourceFolder\$fileName" -Destination $targetFolder
 }
 
 function createArrayFromString ($string) {
     $arrayFromString = $string.Split(";",[System.StringSplitOptions]::RemoveEmptyEntries) 
     [int] $arrayCounter = 0
     foreach ($string in $arrayFromString) {
-    $arrayFromString[$arrayCounter] = $string.trim()
-    $arrayCounter++
+        $arrayFromString[$arrayCounter] = $string.trim()
+        $arrayCounter++
     }
     return ,$arrayFromString #quote is important for keeping array data type when array is empty
 }
@@ -170,13 +155,45 @@ function isAnyStringFromArrayInFile ($file, $stringArray) {
         return $false
     }
     else {
-    foreach ($string in $stringArray)  {
-    $searchedString = Get-Content "$inputFolder\$file" -encoding $inputFilesEncoding | Select-String $string
-    if ($searchedString -ne $null) {
-        return $true
+        foreach ($string in $stringArray)  {
+            $searchedString = Get-Content "$inputFolder\$file" -encoding $inputFilesEncoding | Select-String $string
+            if ($searchedString -ne $null) {
+                return $true
+            }
+        }
+        return $false
     }
+}
+
+function getRowNumberOfText ($file, $search) {
+    $searchedLine = Get-Content "$inputFolder\$file" -encoding $inputFilesEncoding | Select-String $search
+    if ($searchedLine -eq $null) {
+        return $null
     }
-    return $false
+    elseif ($searchedLine.count -gt 1) {
+        return "MultipleRowsFoundError"
+    }
+    else {
+        [int] $lineNumber = $searchedLine.LineNumber
+        return $lineNumber
+    }
+}
+
+function createEmptiedFileInFolder ($file, $sourceFolder, $targetFolder) {
+    $fileName = $file.Name
+    copyFileToFolder $file $sourceFolder $targetFolder
+    Clear-Content "$targetFolder\$fileName"
+}
+
+function createFileFromArray ($file, $array, $folder) {
+    $fileName = $file.Name
+    if ($inputFilesEncoding.ToLower() -eq 'utf8') {
+        $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+        [System.IO.File]::WriteAllLines("$folder\$fileName", $array, $Utf8NoBomEncoding)
+    }
+    else {
+        $newFile = New-Item "$folder\$fileName" -ItemType file 
+        $array | Out-File $newFile -Encoding $inputFilesEncoding
     }
 }
 
@@ -239,7 +256,7 @@ function processFile ($file) {
       createFileFromArray $file $scriptHeadArray $outputFolder
 
       # Insering source system/-s to output file
-      for ([int]$i=0; $i -lt $SourceSystemStartLineNumbers.count; $i++) {
+      for ($i=0; $i -lt $SourceSystemStartLineNumbers.count; $i++) {
         [System.Collections.ArrayList]$sourceSystemArray = @(Get-Content "$inputFolder\$file" -encoding $inputFilesEncoding)
         $count1 = $SourceSystemStartLineNumbers[$i] - 1
         $sourceSystemArray.RemoveRange(0,$count1)
@@ -251,7 +268,7 @@ function processFile ($file) {
 
       # Insering FOOT from input file to output file
       [System.Collections.ArrayList]$scriptFootArray = @(Get-Content "$inputFolder\$file" -encoding $inputFilesEncoding)
-      [int] $footStart = $scriptEnd - 1
+      $footStart = $scriptEnd - 1
       $scriptFootArray.RemoveRange(0,$footStart)
       Add-Content -Path "$outputFolder/$file" -Value $scriptFootArray -encoding $inputFilesEncoding
 
@@ -259,16 +276,4 @@ function processFile ($file) {
     }
 }
 
-function createLogFile () {
-    $Stamp = (Get-Date).toString("dd-MM-yyyy HHmmss")
-    $logFileName = "run." + $Stamp -replace " ","."
-    $newLogFile = New-Item "$logFileFolder\$logFileName.log" -ItemType file 
-    return $newLogFile
-}
-
-function logWrite ([string]$message) {
-    $Stamp = (Get-Date).toString("dd.MM.yyyy HH:mm:ss")
-    Add-Content $logFile -Value "$Stamp     $message"
-    Write-Host $message
-}
 
--- src/parameters.ps1
+++ src/parameters.ps1
@@ -1,17 +1,28 @@
-$sourceSystem = "AIA4;ENUM"
+<## PARAMETERS - for additional info about each parameter check documentation ##>
 
+<# File Processing Parameters #>
+$sourceSystem = "AIA4;ENUM"
 $ignoreStrings = ""
 $copyStrings = ""
-
-$fromOwnInputFolder = $false
-$ownInputFolder = "C:\git\I_Phase"
-$createFilesInMainFolder = $true
-
 $inputFilesEncoding = "utf8"
 
+
+<# Folder Management Parameters #>
+$mainFolder = "C:\git\I_Phase_selected"
+$fromOwnInputFolder = $false
+$ownInputFolder = "C:\git\I_Phase"
+$createFilesForIFPC = $true
+$IFPCFolder = "C:\git\I_Phase_Selected"
+
+
+<# Conversion Parameters #>
 $isConversionWanted = $true
 $doOnlyConversion = $false
-$ownConversionDefinitionFile = "conversion.cfg"
+$targetEnvironment = ""
+$productionDataLoad = $false
+$ownConversionDefinitionFile = $false
+$ConversionDefinitionFile = ""
+
 
 . "$mainFolder\src\functions.ps1"
 Main
